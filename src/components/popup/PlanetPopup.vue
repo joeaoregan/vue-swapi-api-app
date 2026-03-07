4e1b9d76a9aa4d7efe955932d6f3343aa9b7b1be
joeaoregan <joe.oregan@gmail.com>
2026-03-07 13:07:00 +0000

    refactor PlanetPopup

--- src/components/popup/PlanetPopup.vue
+++ src/components/popup/PlanetPopup.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="popup" @click.self="$emit('toggle-planet')">
+  <div class="popup" @click.self="emit('toggle-planet')">
     <transition name="popup-fade">
       <div class="popup-inner">
         <h1>{{ planet.name }}</h1>
 
         <p>
-          <strong>Diameter:</strong> {{ formatStringOrNumber(planet.diameter) }}<br />
-          <strong>Climate:</strong> {{ planet.climate }}<br />
-          <strong>Population:</strong> {{ formatStringOrNumber(planet.population) }}
+          <strong>Diameter:</strong> {{ formatStringOrNumber(planet.diameter) }}
+        </p>
+        <p><strong>Climate:</strong> {{ planet.climate }}</p>
+        <p>
+          <strong>Population:</strong>
+          {{ formatStringOrNumber(planet.population) }}
         </p>
 
-        <button class="button" @click="$emit('toggle-planet')">Close</button>
+        <button class="button" @click="emit('toggle-planet')">Close</button>
       </div>
     </transition>
   </div>
 </template>
 
 <script setup>
+import { onMounted, onUnmounted } from "vue";
+
 const props = defineProps({
   planet: Object,
 });
+
+const emit = defineEmits(["toggle-planet"]);
 
 function capitalFirstLetter(word) {
   return word ? word.charAt(0).toUpperCase() + word.slice(1) : "";
@@ -30,6 +37,20 @@ function formatStringOrNumber(value) {
     ? parseInt(value).toLocaleString()
     : capitalFirstLetter(value);
 }
+
+function handleKeydown(event) {
+  if (event.key === "Escape") {
+    emit("toggle-planet");
+  }
+}
+
+onMounted(() => {
+  window.addEventListener("keydown", handleKeydown);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", handleKeydown);
+});
 </script>
 
 <style src="./style.css"></style>
