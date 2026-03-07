d8a19cace63ad7824bdf7663414d4796574b7a50
joeaoregan <joe.oregan@gmail.com>
2026-03-07 02:03:55 +0000

    Clean up Home.vue logic and extract global styles into separate file

--- src/Home.vue
+++ src/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <img :alt="imgAlt" src="./assets/logo.png" width="100" />
+
     <form v-show="people != null" id="search">
       Search <s>Your Feelings</s> Users:
       <input name="query" v-model="searchQuery" />
@@ -13,8 +14,7 @@
       :columns="gridColumns"
       :filter-key="searchQuery"
       :ready="ready"
-    >
-    </UserTable>
+    />    
 
     <PlanetPopup
       v-if="displayPopup"
@@ -23,7 +23,7 @@
       :diameter="planet.diameter"
       :climate="planet.climate"
       :population="planet.population"
-    ></PlanetPopup>
+    />
   </div>
 </template>
 
@@ -39,31 +39,11 @@ import axios from "axios";
 
 export default {
   name: "App",
-  metaInfo: {
-    title: "New Title",
-  },
-  setup() {
-    return {
-      PlanetPopup,
-    };
-  },
+
   components: {
     UserTable,
   },
-  methods: {
-    testFunc(n) {
-      this.testStr = n;
-    },
-    togglePopup(planet) {
-      if (planet != null) {
-        this.planet.name = planet.name;
-        this.planet.diameter = planet.diameter;
-        this.planet.climate = planet.climate;
-        this.planet.population = planet.population;
-      }
-      this.displayPopup = !this.displayPopup;
-    },
-  },
+
   data() {
     return {
       ready: false,
@@ -75,67 +55,54 @@ export default {
       gridColumns: ["name", "height", "mass", "created", "edited", "homeworld"],
     };
   },
-  mounted() {
+
+  methods: {
+    togglePopup(planet) {
+      if (planet != null) {
+        this.planet.name = planet.name;
+        this.planet.diameter = planet.diameter;
+        this.planet.climate = planet.climate;
+        this.planet.population = planet.population;
+      }
+      this.displayPopup = !this.displayPopup;
+    },
+
+    async loadPeople() {
+      let nextUrl = "https://swapi.dev/api/people/";
+
+      while (nextUrl) {
+        const response = await axios.get(nextUrl);
+        this.people.push(...response.data.results);
+        nextUrl = response.data.next;
+        console.log("People page loaded");
+      }
+
+      console.log("All users loaded");
+    },
+
+    async loadPlanets() {
+      let nextUrl = "https://swapi.dev/api/planets/";
+
+      while (nextUrl) {
+        const response = await axios.get(nextUrl);
+        this.planets.push(...response.data.results);
+        nextUrl = response.data.next;
+        console.log("Planets page loaded");
+      }
+
+      console.log("All planets loaded");
+    },
+
+  },
+
+  async mounted() {
     document.title = "Joe O'Regan SWAPI App";
 
-    let nextPersonPage = "https://swapi.dev/api/people/";
+    await Promise.all([this.loadPeople(), this.loadPlanets()]);
 
-    (async () => {
-      while (nextPersonPage != null) {
-        await axios
-          .get(nextPersonPage)
-          .then((response) => {
-            this.people = this.people.concat(response.data.results);
-            nextPersonPage = response.data.next;
-          })
-          .catch((errors) => {
-            console.error(errors);
-          })
-          .finally(() => {
-            console.log("People Page Loaded");
-          });
-      }
-      console.log("All Users Loaded");
-      checkReady();
-    })();
+    this.ready = true;
+    console.log("Ready:", this.ready);
 
-    let planetPage = 1;
-    let nextPlanetPage = "https://swapi.dev/api/planets/";
-
-    (async () => {
-      while (nextPlanetPage) {
-        await axios
-          .get(nextPlanetPage)
-          .then((response) => {
-            this.planets = this.planets.concat(response.data.results);
-            nextPlanetPage = response.data.next;
-          })
-          .catch((errors) => {
-            console.error("I have a bad feeling about this: " + errors);
-          })
-          .finally(() => console.log("Planets Page " + planetPage + " Loaded"));
-      }
-      console.log("All Home Planets Loaded");
-      checkReady(); 
-    })();
-
-    const checkReady = () => {
-      if (!nextPlanetPage && !nextPersonPage) {
-        this.ready = true;
-        console.log("Ready:", this.ready);
-      }
-    };
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
